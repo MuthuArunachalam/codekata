9a5ad8da088a88fe18a960d9fbbe616e64db7499
MuthuArunachalam <MuthuArunachalam@users.noreply.github.com>
2017-06-21 16:54:31 +0530

    Create greatest among 3.py

---
+++ greatest among 3.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the 1st value "))
+b=int(input("Enter the 2nd value "))
+c=int(input("Enter the 3rd value "))
+if (a>b):
+       if (a>c):
+              print("The is greatest ",a)
+       else:
+              print("The is greatest ",c)
+else:
+       if (b>c):
+              print("The is greatest ",b)
+       else:
+              print("The is greatest ",c)
